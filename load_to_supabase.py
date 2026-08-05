f89da0a26547bab060e36c46b454c8bd4ceedb50
Ruvarashe Moyo <ruvarashe.m@usapschool.org>
2026-08-05 16:01:00 -0400

    Initial commit

--- load_to_supabase.py
+++ load_to_supabase.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-supabase = create_client(url, key)
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 print("Connected to Supabase!")
 
